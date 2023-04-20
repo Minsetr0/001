f98e9f95697b89ff66ac3affff360b2c39bc0553
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-20 18:18:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,27 @@
 ﻿int[,] array = { { 7, 4, 3, 7, 9 }, { 1, 5, 4, 3, 2, }, { 6, 8, 9, 4, 1 }, { 4, 1, 3, 2, 7 }, { 7, 4, 3, 2, 1 } };
 int sumInArray = 0;
 int multiplicationInArray = 1;
-int firstMeasurementInArray = 0;
-int secondMeasurementInArray = 1;
-int secondLine = 1;
-int firstColumn = 0;
+int lineIndex = 1;
+int columnIndex = 0;
 
-for (int i = 0; i < array.GetLength(firstMeasurementInArray); i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-	for (int j = 0; j < array.GetLength(secondMeasurementInArray); j++)
-	{
-		if (firstColumn == j)
-		{
-			multiplicationInArray *= array[i, j];
-		}
-
+	for (int j = 0; j < array.GetLength(1); j++)
+    { 
 		Console.Write(array[i,j] + " ");
 	}
 
 	Console.WriteLine();
 }
 
-for (int i = 0; i < array.GetLength(firstMeasurementInArray); i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(secondMeasurementInArray); j++)
-    {
-        if (secondLine == i)
-        {
-            sumInArray += array[i, j];
-        }
+	multiplicationInArray *= array[i, columnIndex];
+}
 
-        Console.Write(array[i, j] + " ");
-    }
-
-    Console.WriteLine();
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    sumInArray += array[lineIndex, i];
 }
 
 Console.WriteLine($"Сумма второй строки равна {sumInArray}, а произведение первого столбца равно {multiplicationInArray}.");
